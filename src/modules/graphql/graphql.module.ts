424cabea4f3836b26ae0ab0b1d4154d54b2ec444
Hoang Nguyen <nguyenlamhoang7146@gmail.com>
2025-06-15 16:43:44 +0700

    Fix GraphQL schema file path and add schema definition

--- src/modules/graphql/graphql.module.ts
+++ src/modules/graphql/graphql.module.ts
@@ -1,4 +1,3 @@
-
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
@@ -8,7 +7,7 @@ import { join } from 'path';
 	imports: [
 		GraphQLModule.forRoot<ApolloDriverConfig>({
 			driver: ApolloDriver,
-			autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+			autoSchemaFile: join(process.cwd(), 'src/modules/graphql/schema.gql'),
 			sortSchema: true,
 		}),
 	],
