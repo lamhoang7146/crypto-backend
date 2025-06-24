6750369f94d449febdc072eac4cdf61005075675
Hoang Nguyen <nguyenlamhoang7146@gmail.com>
2025-06-24 12:01:28 +0700

    Remove Todo module, service, resolver, DTOs, and entity files

--- src/modules/todo/todo.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { TodoService } from '@/modules/todo/todo.service';
-import { TodoResolver } from '@/modules/todo/todo.resolver';
-import { PrismaService } from '@/modules/prisma/prisma.service';
-
-@Module({
-  providers: [TodoResolver, TodoService, PrismaService],
-})
-export class TodoModule { }
