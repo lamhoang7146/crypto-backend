962c4ab934eeca5ffc6baa0f63d36024d7c75c11
Lam Hoang <hoang.nguyen@stunited.vn>
2025-10-02 22:49:23 +0700

    refactor: consolidate module imports into AppModules array for cleaner AppModule structure

--- src/modules/app/app.module.ts
+++ src/modules/app/app.module.ts
@@ -1,25 +1,7 @@
 import { Module } from '@nestjs/common';
-import {
-  AuthModule,
-  CommentModule,
-  ConfigCustomModule,
-  GraphQlModule,
-  LikeModule,
-  PostModule,
-  TagModule,
-  UserModule,
-} from '@/modules/app/module';
+import { AppModules } from '@/modules/app/module';
 
 @Module({
-  imports: [
-    ConfigCustomModule,
-    GraphQlModule,
-    UserModule,
-    AuthModule,
-    PostModule,
-    TagModule,
-    CommentModule,
-    LikeModule,
-  ],
+  imports: [...AppModules],
 })
 export class AppModule {}
--- src/modules/app/module.ts
+++ src/modules/app/module.ts
@@ -1,8 +1,19 @@
-export * from '@/modules/graphql/graphql.module';
-export * from '@/modules/user/user.module';
-export * from '@/modules/post/post.module';
-export * from '@/modules/tag/tag.module';
-export * from '@/modules/comment/comment.module';
-export * from '@/modules/like/like.module';
-export * from '@/modules/auth/auth.module';
-export * from '@/modules/config/config.module';
+import { GraphQlModule } from '@/modules/graphql/graphql.module';
+import { UserModule } from '@/modules/user/user.module';
+import { PostModule } from '@/modules/post/post.module';
+import { TagModule } from '@/modules/tag/tag.module';
+import { CommentModule } from '@/modules/comment/comment.module';
+import { LikeModule } from '@/modules/like/like.module';
+import { AuthModule } from '@/modules/auth/auth.module';
+import { ConfigCustomModule } from '@/modules/config/config.module';
+
+export const AppModules = [
+  ConfigCustomModule,
+  GraphQlModule,
+  UserModule,
+  AuthModule,
+  PostModule,
+  TagModule,
+  CommentModule,
+  LikeModule,
+];
